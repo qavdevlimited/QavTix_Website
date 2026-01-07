3d372d4b6274bda4708fef12a5ed0c43cf8581d0
Nelson Onuegbu <leonenelson38@gmail.com>
2026-01-07 08:26:36 +0100

    Done with EventCategory Section, creating Eventcategory section

--- src/components/homepage/EventCategoryBox.tsx
+++ src/components/homepage/EventCategoryBox.tsx
@@ -1,33 +1,30 @@
 import { CustomIcons } from "../Svg-Icons";
-import { Label } from "../ui/label";
 import { space_grotesk } from "@/lib/redux/fonts";
 import { EventCategory, EventCategoryValue, getEventCategoryLabel } from "@/components-data/eventCategory";
 
 export default function EventCategoryBox({ value }: { value: EventCategoryValue }) {
 
     return (
-        <Label htmlFor={value} className="cursor-pointer block w-full">
-            <div className="bg-gray-200 rounded-xl h-52 px-6 py-4 flex flex-col justify-center items-center border-2 border-transparent hover:border-primary-5 transition-colors">
-                <div className="space-y-3">
-                    {
-                        value === EventCategory.ConcertAndMusic.value ? <CustomIcons.musicConcerts /> :
-                        value === EventCategory.SportAndFitness.value ? <CustomIcons.sportAndFitness /> : 
-                        value === EventCategory.ArtAndTheater.value ? <CustomIcons.artsAndTheater /> : 
-                        value === EventCategory.FoodAndDrinking.value ? <CustomIcons.foodAndDrink /> : 
-                        value === EventCategory.Festivals.value ? <CustomIcons.festival /> : 
-                        value === EventCategory.BusinessAndNetworking.value ? <CustomIcons.briefCase /> : 
-                        value === EventCategory.TravelsAndTours.value ? <CustomIcons.travel /> : 
-                        value === EventCategory.NightlifeAndParties.value ? <CustomIcons.djIcon />
-                        :
-                        null
-                    }
-                    <div>
-                        <h3 className={`${space_grotesk.className} text-lg font-medium capitalize`}>
-                            {getEventCategoryLabel(value)}
-                        </h3>
-                    </div>
+        <div className="bg-neutral-3 rounded-[18.4px] flex justify-center items-center h-52 px-6 py-4 border-2 border-transparent hover:scale-105 duration-200 transition-all ease-linear">
+            <div className="items-center gap-3 flex flex-col justify-center">
+                {
+                    value === EventCategory.ConcertAndMusic.value ? <CustomIcons.musicConcerts className="size-14" /> :
+                    value === EventCategory.SportAndFitness.value ? <CustomIcons.sportAndFitness className="size-14" /> : 
+                    value === EventCategory.ArtAndTheater.value ? <CustomIcons.artsAndTheater className="size-14" /> : 
+                    value === EventCategory.FoodAndDrinking.value ? <CustomIcons.foodAndDrink className="size-14" /> : 
+                    value === EventCategory.Festivals.value ? <CustomIcons.festival className="size-14" /> : 
+                    value === EventCategory.BusinessAndNetworking.value ? <CustomIcons.briefCase className="size-14" /> : 
+                    value === EventCategory.TravelsAndTours.value ? <CustomIcons.travel className="size-14" /> : 
+                    value === EventCategory.NightlifeAndParties.value ? <CustomIcons.djIcon className="size-14" />
+                    :
+                    null
+                }
+                <div>
+                    <h3 className={`${space_grotesk.className} text-neutral-9 text-sm text-center capitalize`}>
+                        {getEventCategoryLabel(value)}
+                    </h3>
                 </div>
             </div>
-        </Label>
+        </div>
     )
 }
--- src/components/homepage/EventCategorySection.tsx
+++ src/components/homepage/EventCategorySection.tsx
@@ -1,13 +1,16 @@
 import { EVENT_CATEGORIES_ARRAY } from "@/components-data/eventCategory";
 import EventCategoryBox from "./EventCategoryBox";
+import { space_grotesk } from "@/lib/redux/fonts";
 
 export default function EventCategorySection(){
     return (
-        <section>
-            <h2>We’ve got you!</h2>
-            <p>No one gets left out. We’ve got something for everyone, and finding events that make your vibe has never been easier.</p>
+        <section className="mt-16 global-px">
+            <div className="text-center">
+                <h2 className={`${space_grotesk.className} font-bold text-[2rem] text-secondary-9 mb-3`}>We’ve got you!</h2>
+                <p className="text-sm text-neutral-8">No one gets left out. We’ve got something for everyone, and finding events that make your vibe has never been easier.</p>
+            </div>
 
-            <div>
+            <div className="grid grid-cols-2 gap-x-5 gap-y-7 mt-14 px-4 md:px-0">
                 {
                     EVENT_CATEGORIES_ARRAY.map((v,index) => (
                         <EventCategoryBox value={v.value} key={`${v.value}-${index}`} />
